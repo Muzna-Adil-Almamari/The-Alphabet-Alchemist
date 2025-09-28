bcf8dec070f562e980a2278f1037e54cdef8cbb9
Muzna-Adil-Almamari <muznaadilalmamari@gmail.com>
2025-09-28 14:51:40 +0400

    test_case

---
+++ test_case.py
@@ -0,0 +1,15 @@
+examples = [
+    ("aa",[1]),
+    ("abbcc",[2,6]),
+    ("dz_a_aazzaaa",[28,53,1]),
+    ("a_",[0]),
+    ("abcdabcdab",[2,7,7]),
+    ("abcdabcdab_",[2,7,7,0]),
+    ("zdaaaaaaaabaaaaaaaabaaaaaaaabbaa",[34]),
+    ("zza_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_a_",[26]),
+    ("za_a_a_a_a_a_a_a_a_a_a_a_a_azaaa",[40,1]),
+    ("_",[0]),
+    ("_ad",[0]),
+    ("_zzzb",[0]),
+    ("__",[0]),
+]
